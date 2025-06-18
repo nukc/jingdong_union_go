6895dbe50331d7b206535f42442826f5614b6367
C <bocause@qq.com>
2025-06-18 16:18:14 +0800

    feat: 京享红包效果数据, 创建推广位

---
+++ jd.union.open.position.create.go
@@ -0,0 +1,56 @@
+package jingdong_union_go
+
+import (
+	"encoding/json"
+	"log"
+)
+
+type JdUnionOpenPositionCreateResponse struct {
+	JdUnionOpenPositionCreateResponce JdUnionOpenPositionCreateResponce `json:"jd_union_open_position_create_responce"`
+}
+
+type JdUnionOpenPositionCreateResponce struct {
+	CreateResult JdUnionOpenPositionCreateResult `json:"createResult"`
+}
+
+type JdUnionOpenPositionCreateResult struct {
+	Code    string                        `json:"code"`
+	Message string                        `json:"message"`
+	Data    JdUnionOpenPositionCreateData `json:"data"`
+}
+
+type JdUnionOpenPositionCreateData struct {
+	UnionId    string     `json:"unionId"`
+	SiteId     string     `json:"siteId"`
+	Pid        PidObject  `json:"pid"`
+	Type       string     `json:"type"`
+	ResultList ResultList `json:"resultList"`
+}
+
+type PidObject struct {
+	Pid string `json:"pid"`
+}
+
+type ResultList struct {
+	Result string `json:"result"`
+}
+
+// JdUnionOpenPositionCreate 创建推广位
+func (app *App) JdUnionOpenPositionCreate(params map[string]interface{}) (result *JdUnionOpenPositionCreateResult, err error) {
+
+	body, err := app.Request("jd.union.open.statistics.redpacket.query", "2.0", map[string]interface{}{"effectDataReq": params})
+
+	resp := &JdUnionOpenPositionCreateResponse{}
+	if err != nil {
+		log.Println(string(body))
+		return
+	}
+
+	if err = json.Unmarshal(body, resp); err != nil {
+		return
+	}
+
+	result = &resp.JdUnionOpenPositionCreateResponce.CreateResult
+
+	return
+}
---
+++ jd.union.open.statistics.redpacket.query.go
@@ -0,0 +1,59 @@
+package jingdong_union_go
+
+import (
+	"encoding/json"
+	"log"
+)
+
+type JdUnionOpenStatisticsRedpacketQueryResponse struct {
+	JdUnionOpenStatisticsRedpacketQueryResponce JdUnionOpenStatisticsRedpacketQueryResponce `json:"jd_union_open_statistics_redpacket_query_responce"`
+}
+
+type JdUnionOpenStatisticsRedpacketQueryResponce struct {
+	QueryResult JdUnionOpenStatisticsRedpacketQueryQueryResult `json:"queryResult"`
+}
+
+type JdUnionOpenStatisticsRedpacketQueryQueryResult struct {
+	Code       string                                       `json:"code"`
+	Message    string                                       `json:"message"`
+	TotalCount string                                       `json:"totalCount"`
+	Data       JdUnionOpenStatisticsRedpacketQueryQueryData `json:"data"`
+}
+
+type JdUnionOpenStatisticsRedpacketQueryQueryData struct {
+	RedPacketEffectDataResp RedPacketEffectDataResp `json:"redPacketEffectDataResp"`
+}
+
+type RedPacketEffectDataResp struct {
+	ShowNum       string `json:"showNum"`
+	PromotionName string `json:"promotionName"`
+	PositionId    string `json:"positionId"`
+	UseNum        string `json:"useNum"`
+	ActId         string `json:"actId"`
+	GiveNum       string `json:"giveNum"`
+	OrderNum      string `json:"orderNum"`
+	OrderFee      string `json:"orderFee"`
+	OrderPrice    string `json:"orderPrice"`
+	TjDate        string `json:"tjDate"`
+	ChannelId     string `json:"channelId"`
+}
+
+// JdUnionOpenStatisticsRedpacketQuery 京享红包效果数据
+func (app *App) JdUnionOpenStatisticsRedpacketQuery(params map[string]interface{}) (result *JdUnionOpenStatisticsRedpacketQueryQueryResult, err error) {
+
+	body, err := app.Request("jd.union.open.statistics.redpacket.query", "2.0", map[string]interface{}{"effectDataReq": params})
+
+	resp := &JdUnionOpenStatisticsRedpacketQueryResponse{}
+	if err != nil {
+		log.Println(string(body))
+		return
+	}
+
+	if err = json.Unmarshal(body, resp); err != nil {
+		return
+	}
+
+	result = &resp.JdUnionOpenStatisticsRedpacketQueryResponce.QueryResult
+
+	return
+}
